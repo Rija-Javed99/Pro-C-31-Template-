d6d78878e737bdbbb8e5d073b55805a550dc46ad
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-07 15:50:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,13 @@ var Engine = Matter.Engine,
   Events = Matter.Events,
   Bodies = Matter.Bodies;
  
-var particles = [];
-var plinkos = [];
-var divisions =[];
-var divisionHeight=300;
+//create arrays
+
+//div height as 300
+
+
 var score =0;
+
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
@@ -15,37 +17,11 @@ function setup() {
   ground = new Ground(width/2,height,width,20);
 
 
-   for (var k = 0; k <=width; k = k + 80) {
-     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-   }
+ //create for loop for the divisions
+  
+  
 
-
-    for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,175));
-    }
-
-     for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,275));
-    }
-
-     for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,375));
-    }
-
-    
-
+//create for loops for diff colored plinkos
     
 }
  
@@ -53,27 +29,14 @@ function setup() {
 
 function draw() {
   background("black");
-  textSize(20)
- //text("Score : "+score,20,30);
+
   Engine.update(engine);
   ground.display();
   
-   for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
    if(frameCount%60===0){
      particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
      score++;
    }
  
-  for (var j = 0; j < particles.length; j++) {
-   
-     particles[j].display();
-   }
-   for (var k = 0; k < divisions.length; k++) {
-     
-     divisions[k].display();
-   }
+  //display plinkos in loops
 }
